049c702593ecf9b7342d1169a505bc48d019d372
tzminch <71556663+tzminch@users.noreply.github.com>
2020-12-26 17:29:29 +0800

    trials

--- Trial_1.py
+++ Trial_1.py
@@ -2,6 +2,7 @@
 # -*- coding:utf-8 -*-
 
 import json
+from ArticutAPI import ArticutAPI
 
 def createJson(jsonPath, inputDICT):
     with open(jsonPath, 'w', encoding='utf-8') as f:
@@ -21,4 +22,29 @@ if __name__== "__main__":
     adjDICT["negative"] = negLIST
     
     createJson('./adjectives.json', adjDICT)
+    
+    with open("民間文學與說唱藝術_蔡孟珍.txt", encoding="utf-8") as f:
+        inputSTR = f.read()    
+    articut = ArticutAPI.Articut()
+    resultDICT = articut.parse(inputSTR)
+    #resultLIST = articut.getNounStemLIST(resultDICT)
+    #print(resultDICT)
+    resultLIST = resultDICT["result_segmentation"]
+    refLIST = resultLIST.split("/")
+    
+    score = 0
+    for i in refLIST:
+        for j in posLIST:
+            if i == j:
+                score += 1
+        for k in negLIST:
+            if i == k:
+                score -= 1
+    print("此門課分數為：", score)
+    
+    if score > 0:
+        print("正面")
+    else:
+        print("負面")
+
     
